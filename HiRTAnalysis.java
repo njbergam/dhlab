44736cb1eab00b33817599cdc49fcdd4412e9c39
Noah Bergam <nbergam2021@pingry.org>
2019-01-29 10:46:19 -0500

    first commit

---
+++ HiRTAnalysis.java
@@ -0,0 +1,42 @@
+public class HiRTAnalysis{
+
+  public static void main(String[] args){
+
+  }
+}
+
+class Chapter{
+  private String filename;
+  private String narrator;
+  private WordFrequency wf;
+  private SentenceStructure ss;
+
+
+
+}
+
+class WordFrequency{
+
+  // keeps dictionary of all occurences of all words
+
+  public int numOccurrences(String word){
+    return 0;
+  }
+
+}
+
+class SentenceStructure{
+  //count numbers of types of sentences (complex, simple, non-sentences)
+
+
+}
+
+class Readability{
+  //use different readibility index algorithms on the chapter
+}
+
+class Character{
+  private boolean isNarrator;
+  private boolean isMale;
+
+}
