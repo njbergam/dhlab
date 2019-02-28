6dcd6f7336e1c0017aad763846344f045efba588
Noah Bergam <nbergam2021@pingry.org>
2019-02-27 21:51:35 -0500

    now Python

---
+++ Analysis.py
@@ -0,0 +1,13 @@
+#Python Stuff
+
+def fleischReadingTest():
+    return 0
+
+def readabilityEase():
+    return 0
+
+def averageSentenceLength():
+    return 0
+
+def avgNumSyllables():
+    return 0
