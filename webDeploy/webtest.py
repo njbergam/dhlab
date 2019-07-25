b78b56e1cb7e0e5f9626c05d9a8a7dd38e7c036c
JulianLee123 <jlee2021@pingry.org>
2019-07-25 10:37:47 -0400

    Update webtest.py

--- webDeploy/webtest.py
+++ webDeploy/webtest.py
@@ -3,14 +3,15 @@ from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 from flask import render_template
 
-from test import cleanText
-from test import simpleTokenize
-from test import detokenize
-from test import percentQuotes
-from test import senlenStats
-from test import savePOSPiChart
-from test import deList
-from test import saveTopWords
+from simpleFunctions import cleanText
+from simpleFunctions import simpleTokenize
+from simpleFunctions import detokenize
+from simpleFunctions import percentQuotes
+from simpleFunctions import senlenStats
+from simpleFunctions import savePOSPiChart
+from simpleFunctions import deList
+from simpleFunctions import saveTopWords
+from simpleFunctions import getWordFreqDict
 
 from thesis import thesisVector
 
@@ -25,17 +26,25 @@ from reports import sampleCharacter
 import random
 import string
 
+import nltk
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+nltk.download('universal_tagset')
+
+branch = '/Users/JulianMacBookPro/Desktop/AmericanModernism/webDeploy'
 
 
-app = Flask(__name__, static_folder=os.path.abspath('/Users/nbergam/Desktop/AmericanModernism/webDeploy/templates/static') )
-
+priorUrl = '/single'
+fname = "abc"
+app = Flask(__name__, static_folder=os.path.abspath(branch+'/templates/static') )
 if __name__ == '__main__':
     app.run(debug=True)
     app.debug = True
     app.secret_key = "vkjgvkgv"
 
-UPLOAD_FOLDER = '/Users/nbergam/Desktop/AmericanModernism/webDeploy/uploads'
-GRAPHS_FOLDER = '/Users/nbergam/Desktop/AmericanModernism/webDeploy/graphs'
+UPLOAD_FOLDER = branch + '/uploads'
+GRAPHS_FOLDER = branch + 'graphs'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
@@ -50,8 +59,9 @@ def allowed_file(filename):
 def home():
     return render_template('hello.html')
 
-@app.route('/<type>', methods=['GET', 'POST'])
-def upload_file(type):
+@app.route('/upload_file', methods=['GET', 'POST'])
+def upload_file():
+    global fname
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -64,16 +74,17 @@ def upload_file(type):
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            fname = secure_filename(file.filename)
+            print( fname)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
             # where you go after uploading
-            return redirect(url_for('get_file',
-                                    filename=filename))
-    return render_template(type+'.html')
+            global priorUrl
+            return redirect(priorUrl)
+    return redirect(request.url)#render_template(type+'.html')
 
 from flask import send_from_directory
 
-@app.route('/Users/nbergam/Desktop/AmericanModernism/webDeploy/uploads/<filename>')
+@app.route(branch+'/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
@@ -81,36 +92,46 @@ def uploaded_file(filename):
 
 @app.route('/single')
 def single():
-	return render_template('oneText.html')
+    dict = getWordFreqDict(200)
+    global priorUrl
+    priorUrl = '/single'
+    return render_template('oneText.html')
 
 
 @app.route('/report', methods=['GET', 'POST'])
 def get_file():
+    global fname
     # need to retrieve the uploaded file here for further processing
-
+    print( fname)
     print(request.form)
-    filename =  str(request)[ str(request).index('=')+1 : str(request).index('\' [GET]>') ]
-
-    print( filename)
-    text = simpleTokenize( 'uploads/' + filename )
-    text2  = cleanText( 'uploads/' + filename )
-    pos = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
-    savePOSPiChart(text2, pos)
-    top = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+    dict = request.form.to_dict()
+    #filename =  str(request)[ str(request).index('=')+1 : str(request).index('\' [GET]>') ]
+    text = simpleTokenize( 'uploads/' + fname )
+    text2  = cleanText( 'uploads/' + fname )
+    print("creating pos chart")
+    pos = "posChart"#title of the generated chart
+    #savePOSPiChart(text2, pos)
+    print("creating top words chart")
+    top = "topWordChart"
+    #os.unlink(branch+"/templates/static/graphs/" + top + ".png")
+    #item_id = branch+"/templates/static/graphs/" + top + ".png"
+    #item = self.session.query(Item).get(item_id)
+    #self.session.delete(item)
+    #db.session.commit()
+    #os.remove(branch+"/templates/static/graphs/" + top + ".png");
     saveTopWords(text2, top)
-
-
+    """
     bw = ['yellow', 'fish', 'glass', 'foot', 'beach', 'suicide']
     genGraph = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
     secgen = similarContext( text,  bw)
+    print("c")
     saveChronoMap(text, bw, secgen, genGraph)
     genReport = wpReport(text, bw, secgen, 10)
 
     charReport = sampleCharacter(text, 'Caddy', 3, 100)
+    print("d")"""
 
-
-    return render_template('results.html', pq = percentQuotes(text), sen = senlenStats(text), pos = pos, top = top, genGraph=genGraph,genReport=genReport, charReport=charReport)
-
+    return render_template('results.html', pq = percentQuotes(text), sen = senlenStats(text), pos = pos, top = top)
 #---Double Text
 
 @app.route('/double')
@@ -131,7 +152,7 @@ def multi():
 
 @app.route('/thesis')
 def student():
-   return render_template('thesis.html')
+    return render_template('thesis.html')
 
 @app.route('/thesis-result', methods = ['GET', 'POST'])
 def result():
