36078fd8bb75b8967a623709ef89ae7f8834ddab
dhlab <dhlab@dhlab.pingry.org>
2021-05-17 08:54:06 -0400

    now runs with gunicorn

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -30,7 +30,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY='password',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
