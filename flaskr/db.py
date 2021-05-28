c7d34fdfa1e727210543062885b2b582d53cdbbf
Lukas Strelecky <lhstrelecky@gmail.com>
2021-05-28 14:43:35 -0400

    More documentation.

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,3 +1,4 @@
+# SQLITE database engine
 import sqlite3
 
 import click
@@ -5,24 +6,40 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 
+# The first thing to do when working with a SQLite database is to create a connection to
+# it. Any queries and operations are performed using the connection, which is closed after
+# the work is finished.
+
+# Function that gets the database object (using SQLITE)
 def get_db():
+    # g is a special object that is unique for each request - contains
+    # important information about the request
     if 'db' not in g:
+        # If the database has not been initialized, then do it
         g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
+            # Won't create/initiate the file until later
+            current_app.config['DATABASE'], # Defined in __init__.py
             detect_types=sqlite3.PARSE_DECLTYPES
         )
+
+        # Return the database as rows that acts like dictionary -- enables access by rows
         g.db.row_factory = sqlite3.Row
 
+    # Returns the final db
     return g.db
 
-
+# Closes the database
 def close_db(e=None):
+    # Gets a reference to the database variable (if it exists)
     db = g.pop('db', None)
 
+    # If the database exists, close it.
     if db is not None:
         db.close()
 
+# Initiate the database
 def init_db():
+    # This will create the new database instance
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
@@ -32,7 +49,7 @@ def init_db():
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
-    """Clear the existing data and create new tables."""
+    # Clear the existing data and create new tables.
     init_db()
     click.echo('Initialized the database.')
 
